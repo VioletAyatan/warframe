381cbbdad3ef40e1906269bc774103becf344803
ankol <2329340232@qq.com>
2024-12-02 20:50:52 +0800

    init

---
+++ src/main/kotlin/com/ankol/warframe/WarframeApplication.kt
@@ -0,0 +1,11 @@
+package com.ankol.warframe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WarframeApplication
+
+fun main(args: Array<String>) {
+    runApplication<WarframeApplication>(*args)
+}
---
+++ src/test/kotlin/com/ankol/warframe/WarframeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ankol.warframe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WarframeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
